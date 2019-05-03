949bb4ba0af3857a39c9dc3a46f77504b83447de
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2019-05-03 21:20:38 +0000

    new version and formatting

--- control.lua
+++ control.lua
@@ -1,30 +1,29 @@
 local Gui = require("gui")
 
-
 local function MakeGlobals()
-    if global.MOD == nil then global.MOD = {} end
-    if global.MOD.guiScreenshotSettings == nil then global.MOD.guiScreenshotSettings = {} end
+    if global.MOD == nil then
+        global.MOD = {}
+    end
+    if global.MOD.guiScreenshotSettings == nil then
+        global.MOD.guiScreenshotSettings = {}
+    end
 end
-
 
 local function PlayerJoinedEvent(eventData)
     local player = game.get_player(eventData.player_index)
     Gui.GenerateDefaultSettings(player)
 end
 
-
 local function OnInit()
     MakeGlobals()
     Gui.CloseGuiForAll()
 end
-
 
 local function OnConfigurationChanged()
     MakeGlobals()
     Gui.GenerateDefaultSettingsForAll()
     Gui.CloseGuiForAll()
 end
-
 
 script.on_init(OnInit)
 script.on_configuration_changed(OnConfigurationChanged)
